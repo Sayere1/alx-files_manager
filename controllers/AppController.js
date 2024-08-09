9fcc86b7e95e3bc3765c834302f4886e0fce454b
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-08-09 03:38:14 -0700

    updated

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,12 +1,10 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-
 class AppController {
   static getStatus(request, response) {
     response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
-
 
   static async getStats(request, response) {
     const usersNum = await dbClient.nbUsers();
